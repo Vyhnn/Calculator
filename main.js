9d3042f8baa37f1397e4a7b48dc14477ff2dc959
Vyhnn <s0609674@gmail.com>
2018-07-31 01:25:19 +0800

    add basic operation

--- main.js
+++ main.js
@@ -1,88 +1,153 @@
-let formula = [];
 const formulaDisplay = document.getElementById("formula");
 const numDisplay = document.getElementById("number-display");
+const operatorsList = ['+', '-', '×', '÷', '='];
+let formula = [];
+let result = 0;
+let currentNum = '';
 
 function add(x, y) {
-	return x+y;
+	return parseFloat(x)+parseFloat(y);
 }
 
 function subtract(x, y) {
-	return x-y;
+	return parseFloat(x)-parseFloat(y);
 }
 
 function divide(x, y) {
-	return x/y;
+	return parseFloat(x)/parseFloat(y);
 }
 
 function multiply(x, y) {
-	return x*y;
+	return parseFloat(x)*parseFloat(y);
 }
 
 function power(x, y) {
-	return Math.pow(x, y);
+	return Math.pow(parseFloat(x), parseFloat(y));
 }
 
 function root(x) {
-  Math.sqrt(x);
+  Math.sqrt(parseFloat(x));
 }
 
 function modulus(x, y) {
-  return x%y;
+  return parseFloat(x)%parseFloat(y);
 }
 
 function clear() {
   formulaDisplay.innerHTML = '';
   numDisplay.innerHTML = '';
   formula = [];
+	currentNum = '';
+	result = '';
 }
 
 function plusmn() {
-  if(!(numDisplay.innerHTML==='')) {
-    let num = parseFloat(numDisplay.innerHTML);
-    console.log(numDisplay.innerHTML);
-    numDisplay.innerHTML = num * -1;
-  }
+  currentNum = parseFloat(currentNum)* -1;
+}
+
+function update() {
+	numDisplay.innerHTML = currentNum;
+	formulaDisplay.innerHTML = formula.join(' ');
+}
+
+function cal() {
+	const operator = formula[formula.length-3];
+	console.log(operator);
+
+	switch (operator) {
+		case '+':
+			result = add(result, currentNum);
+			break;
+		case '-':
+			result = subtract(result, currentNum);
+			break;
+		case '×':
+			result = multiply(result, currentNum);
+			break;
+		case '÷':
+			if(currentNum!=0) {
+				result = divide(result, currentNum);
+			}
+			break;
+		}
+		console.log(result)
 }
 
 function operate(operator) {
-  console.log(operator);
-  switch (operator) {
-    case '+':
-      return add(x, y);
-      break;
-    case '-':
-      return subtract(x, y);
-      break;
-    case '×':
-      return multiply(x, y);
-      break;
-    case '÷':
-      return divide(x, y);
-      break;
-    case '√':
-      return root(x);
-      break;
-    case '%':
-      return modulus(x, y);
-      break;
-    case '^':
-      return power(x, y);
-      break;
-    case 'CE':
-      return clear();
-      break;
-    case '±':
-      return plusmn();
-      break;
-    default:
-      break;
+	if(currentNum!=''||!operatorsList.includes(operator)) {
+	  switch (operator) {
+	    case '+':
+				formula.push(currentNum);
+				formula.push('+');
+				if(formula.length>2) {cal();}
+				else {result = currentNum;}
+				currentNum = '';
+				update();
+				numDisplay.innerHTML = result;
+	      break;
+	    case '-':
+				formula.push(currentNum);
+				formula.push('-');
+				if(formula.length>2) {cal();}
+				else {result = currentNum;}
+				currentNum = '';
+				update();
+				numDisplay.innerHTML = result;
+	      break;
+	    case '×':
+				formula.push(currentNum);
+				formula.push('×');
+				if(formula.length>2) {cal();}
+				else {result = currentNum;}
+				currentNum = '';
+				update();
+				numDisplay.innerHTML = result;
+	      break;
+	    case '÷':
+				formula.push(currentNum);
+				formula.push('÷');
+				if(formula.length>2) {cal();}
+				else {result = currentNum;}
+				currentNum = '';
+				update();
+				numDisplay.innerHTML = result;
+	      break;
+	    case '√':
+	      return root(x);
+	      break;
+	    case '%':
+	      return modulus(x, y);
+	      break;
+	    case '^':
+	      return power(x, y);
+	      break;
+	    case 'CE':
+	      return clear();
+	      break;
+	    case '±':
+	      plusmn();
+				update();
+	      break;
+			case '=':
+				formula.push(currentNum);
+				formula.push('');
+				cal();
+				currentNum = '';
+				formula = [];
+				update();
+				numDisplay.innerHTML = result;
+				result = '';
 
-  }
+	    default:
+	      break;
+
+	  }
+	}
 }
 
 function displayNum(x) {
-  const num = numDisplay.innerHTML;
-  numDisplay.innerHTML = num + x;
+	currentNum += x;
+  numDisplay.innerHTML = currentNum;
 }
 //apply onclick listeners
 const nums = document.getElementsByClassName("num-btn");
